5f1041907a106ca3465d450adfe02d1f916d41c8
Uwancha <sito2alemu@gmail.com>
2023-09-26 11:13:33 +0300

    fix: Add crossOrigin attribute to fix CORB issue

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -6,7 +6,7 @@ function Pokemon({pokemon, flippedCards, handleClick}) {
     
     return (
       <div className="card" style={{pointerEvents: isFliped ? 'none' : 'auto'}} onClick={() => handleClick(pokemon)}>
-        <img src={isFliped ? pokemon.back.url: pokemon.front.url} className="img" />
+        <img src={isFliped ? pokemon.back.url: pokemon.front.url} crossOrigin="anonymous" className="img" />
         <h2>{isFliped ? pokemon.back.id: pokemon.front.id}</h2>
       </div>
     )
