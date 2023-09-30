eff38ae60c74a77096245c3b192fa6fa053439b2
Uwancha <sito2alemu@gmail.com>
2023-09-30 15:50:24 +0300

    feat: Take attempts from the parent and display

--- src/components/scoreboard.jsx
+++ src/components/scoreboard.jsx
@@ -1,10 +1,11 @@
-function ScoreBoard({score, bestScore}) {
+function ScoreBoard({score, bestScore, attempts}) {
     
     return (
         <div className="scoreBoard">
             <div>
                 <h3>Get points by clicking on an image but don't click on any more than once!</h3>
             </div>
+            <div><h3>Attempts: {attempts}</h3></div>
             <div>
                 <h3>Score: {score}</h3>
                 <h3>Best Score: {bestScore}</h3>
