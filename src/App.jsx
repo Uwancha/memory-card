664abaddfa3735951e828b71f8329b0b78ef32a2
Uwancha <sito2alemu@gmail.com>
2023-09-30 16:16:43 +0300

    feat: Implement game ending conditions Add state to dictate the game is over Implement a function to invoke on game restart Render the app conditionally enabling the end of the game

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { getPokemon } from './services/api'
 import { CardList } from './components/CardList'
 import shuffle from './shuffle'
 import ScoreBoard from './components/scoreboard'
+import GameOVer from './components/GameOver'
 
 function App() {
   const [shuffledCards, setShuffledCards] = useState([]);
@@ -11,6 +12,7 @@ function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [attempts, setAttempts] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false);
 
   useEffect(() => {
     getPokemon().then(data => {
@@ -31,7 +33,8 @@ function App() {
 
   function handleCardClick(card) {
     setAttempts(attempts + 1)
-    if (score === 16) {
+
+    if (score === 12) {
       setIsGameOver(true)
     }
     if (selectedCards.includes(card)) {
@@ -54,13 +57,23 @@ function App() {
     setShuffledCards(shuffled); 
   }
 
+  function restartTheGame () {
+    setScore(0)
+    setSelectedCards([])
+    setAttempts(0)
+    setIsGameOver(false)
+  }
+
   return (
     <div>
+      {isGameOver ? ( <GameOVer onRestart={restartTheGame} />) : (
+        <>
       <ScoreBoard score={score} bestScore={bestScore} attempts={attempts}/>
       <CardList 
         pokemon={shuffledCards}
         handleCardClick={handleCardClick}
       />
+      </>)}
     </div>
   )
 }
